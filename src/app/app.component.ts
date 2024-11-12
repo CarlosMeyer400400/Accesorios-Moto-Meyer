1ae84c4f8551e70dd3cb6e21c1c9139a08337f5e
CARLOSMEYER <20210696@uthh.edu.mx>
2024-11-12 02:58:42 -0600

    pwa

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,5 +9,5 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'Accesorios_para_Moto_Meyer';
+  title = 'Accesorios para Moto Meyer';
 }
